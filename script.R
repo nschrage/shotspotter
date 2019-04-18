fc4efb8182759882a2e6f78ee9841d2f57d7680f
Niel Schrage <nschrage@Niels-MacBook-Pro.local>
2019-04-18 14:44:30 -0400

    end of class commit

--- script.R
+++ script.R
@@ -7,22 +7,54 @@ library(tidyverse)
 library(fs)
 library(sf)
 library(lubridate)
+library(ggmap)
+library(ggplot2)
+library(mapproj)
+library(ggthemes)
 
 (tigris_class = "sf")
 
 # Read in Data 
 
-shotspotter <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/San_Francisco_ShotSpotter.csv")
+shotspotter <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/San_Francisco_ShotSpotter.csv", col_types = 
+  cols(
+  Type = col_character(),
+  ID = col_double(),
+  Date = col_character(),
+  Time = col_time(format = ""),
+  Rnds = col_double(),
+  Beat = col_logical(),
+  DISPO = col_logical(),
+  `returned address` = col_character(),
+  Latitude = col_double(),
+  Longitude = col_double()
+))
 
 # Get Shape Files
 
 shapes <- urban_areas(class = "sf") 
-  
-  
-#st_as_sf() %>%
-#st_crs()
 
-#st_as_sf(shotspotter, coords = c("Longitude", "Latitude"), crs = 4326) 
+san_francisco <- shapes %>% 
+  
+  filter(str_detect(NAME10, "San Francisco--Oakland, CA"))
+  
+ 
+shot_locations <- st_as_sf(shotspotter, coords = c("Longitude", "Latitude"), crs = 4326)
+
+# st_crs(shot_locations) --> Not sure what this is doing
+
+ggplot(data = san_francisco) +
+  
+  geom_sf() +
+  
+  geom_sf(data = shot_locations) +
+  
+  theme_map()
+ 
+# st_as_sf() %>%
+# st_crs()
+
+
 
 # How to Push to Shiny App
 
