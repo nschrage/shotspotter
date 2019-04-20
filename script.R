602051ea6758a97336d656f8a1ce00a1af5b1f04
Niel Schrage <nschrage@Niels-MacBook-Pro.local>
2019-04-20 18:33:47 -0400

    making progress, still not positive how to put in shiny

--- script.R
+++ script.R
@@ -11,6 +11,8 @@ library(ggmap)
 library(ggplot2)
 library(mapproj)
 library(ggthemes)
+library(viridis)
+
 
 (tigris_class = "sf")
 
@@ -28,48 +30,102 @@ shotspotter <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/Sa
   `returned address` = col_character(),
   Latitude = col_double(),
   Longitude = col_double()
-))
+)) 
 
-# Get Shape Files
-
-shapes <- urban_areas(class = "sf") 
-
-san_francisco <- shapes %>% 
-  
-  filter(str_detect(NAME10, "San Francisco--Oakland, CA"))
 
 # Get Shape Files a Different Way via the code on Piazza 
+# first look for places in ca
 
-#shapes_2 <- places("ca", class = "sf", cb = TRUE) %>% 
-#  filter(NAME == "SAN FRANCISCO")
+san_francisco <- places("ca", class = "sf", cb = TRUE) %>% 
+  filter(NAME == "San Francisco")
+
+shotspotter <- shotspotter %>% 
+  
+  mutate(Rnds = as.numeric(Rnds)) %>%
+  
+  mutate(Hours = hour(Time)) %>% 
+  
+  filter(Latitude > 37.71) %>% 
+
+# turn locations into sf object in order to graph 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = st_crs(san_francisco))
+
+
+# set up plot
+
+ggplot(data = san_francisco) + 
+  
+  geom_sf() + 
+  
+  geom_sf(data = shotspotter, aes(size = "Rnds", color = "Hours")) +
+  
+  scale_x_continuous(limits = c(-122.52, -122.35)) +
+  
+  scale_y_continuous(limits = c(37.7, 37.85)) +
+
+  theme_map() + 
+  
+  theme_tufte() 
+
+
+  
+  
+  
+  #labs(
+   # title = "working",
+ #   subtitle = "working",
+ #   caption = "working"
+#  ) +
+  
+  #xlab("Longitude") + 
+  #ylab("Latitude") +
   
  
-shot_locations <- st_as_sf(shotspotter, coords = c("Longitude", "Latitude"), crs = st_crs(san_francisco))
+  
+  #scale_fill_viridis() +
+  
 
-# st_crs(shot_locations) --> Not sure what this is doing
+  
 
-# hmm why don't I have an outline of the city behind the dots. 
-
-ggplot(data = san_francisco) +
   
-  geom_sf() +
+  #transition_time(date)
   
-  geom_sf(data = shot_locations) +
-  
-  theme_map() +
-  
-  transition_states()
  
-# st_as_sf() %>%
+ 
+
 
 # what story do I want to tell. 
 
+  # plot by month 
+  # color by time of day?
+  # size = rnds?
+
+# scale_fill_brewer(palette="Blues",
+#labels = c("0-10", "10-50", "50-100", "100-500",
+#           "500-1,000", "1,000-5,000", ">5,000")
 
 
 
 # How to Push to Shiny App
 
-# I prefer to use tigris directly. The class = "sf" argument is often handy . .
-# . as is urban_areas() . . . st_as_sf() . . . st_crs()
+# General organization is to any/all time consuming work (or work that depends
+# on other websites --- like that the Shotspotter site is up) here, write out a
+# final shots.rds file to the Shotspotter directory, and then access that data
+# from the Shiny code.
+
+
+# Now we can try a few plots to make sure things are working. At this point,
+# there are two ways we could go. First, we could make the plot (or animation)
+# we want, save it, and then just stick the saved object into the Shotspotter
+# directory. Second, we could put the raw data into that directory and then do
+# the plotting in the Shiny App.
+
+
+#shapes <- urban_areas(class = "sf") 
+
+#san_francisco <- shapes %>% 
+
+#filter(str_detect(NAME10, "San Francisco--Oakland, CA"))
+
 
 
